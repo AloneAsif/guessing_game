4151d320cc08c3ddb3168af93eca4d77d112ecb4
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-20 19:47:55 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+import chalk from 'chalk';
+
+// Guessing game 
+
+let guess = await inquirer.prompt([{
+    name: 'guess',
+    type: 'number',
+    message: 'Please guess number from 1 to 10.'
+}])
+
+// let guess = 4;
+
+if(guess.guess === 5){
+    console.log(chalk.greenBright('you guessed corret :) '))
+}
+else if(guess.guess > 5 ){
+    console.log(chalk.greenBright('you are high'))
+    
+}
+else{
+    console.log(chalk.blue('you are low'))
+}
